06bf99281de220fed3fd605ada8a733df5936267
Alfonso Ruiz <aerda@MacBook-Pro-de-Alfonso.local>
2022-04-24 13:01:38 -0500

    updating the footer testing, to match wizeline msg.

--- src/components/Footer/Footer.test.js
+++ src/components/Footer/Footer.test.js
@@ -1,3 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import Footer from './Footer';
+
 describe('Footer component', () => {
-  it.todo('add all test for Footer');
+  it('should have the wizeline msg', () => {
+    render(<Footer />);
+
+    const wizelineMsgEl = screen.getByRole('heading', {
+      name: /project created during wizeline academy react testing bootcamp/i
+    });
+
+    expect(wizelineMsgEl).toBeInTheDocument();
+  });
 });
