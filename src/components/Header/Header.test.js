f81d67f610db78ff341d7a7cda0ea473d33892f1
Alfonso Ruiz <aerda@MacBook-Pro-de-Alfonso.local>
2022-04-24 11:59:27 -0500

    start TDD for Header.

---
+++ src/components/Header/Header.test.js
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react';
+import Header from './';
+
+describe('Header component', () => {
+  it('should have the project title', () => {
+    render();
+  });
+});
