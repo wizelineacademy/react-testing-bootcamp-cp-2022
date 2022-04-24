c308cdb03f8769d2e85b7746d7f238437d9c25ba
Alfonso Ruiz <aerda@MacBook-Pro-de-Alfonso.local>
2022-04-24 12:52:12 -0500

    checking the heading title for the project.

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,8 +1,14 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Header from './Header';
 
 describe('Header component', () => {
   it('should have the project title', () => {
     render(<Header />);
+
+    const titleEl = screen.getByRole('heading', {
+      name: /wizeline testing proyect/i
+    });
+
+    expect(titleEl).toBeInTheDocument();
   });
 });
