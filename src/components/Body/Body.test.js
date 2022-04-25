e2ed0257360ca60fe0cd597f1fd10cba6e9e52b2
Alfonso Ruiz <aerda@MacBook-Pro-de-Alfonso.local>
2022-04-24 19:59:59 -0500

    updating first 2 test of the body component.

--- src/components/Body/Body.test.js
+++ src/components/Body/Body.test.js
@@ -1,3 +1,27 @@
+import { render, screen } from '@testing-library/react';
+import Body from './Body';
+
 describe('Body component', () => {
-  it.todo('add all test for Footer');
+  it('should have a date picker', () => {
+    render(<Body />);
+
+    const datePickerEl = screen.getByLabelText(/date picker/i);
+
+    expect(datePickerEl).toBeInTheDocument();
+  });
+  it('should not have the NASA APOD image, title, and date', () => {
+    render(<Body />);
+
+    const imgEl = screen.queryByAltText(/apo img/i);
+    const titleEl = screen.queryByRole('heading');
+    const dateEl = screen.queryByRole('heading', {
+      name: /^\d{2}\/\d{2}\/\d{4}$/
+    });
+
+    // Before the api is used
+    expect(imgEl).not.toBeInTheDocument();
+    expect(titleEl).not.toBeInTheDocument();
+    expect(dateEl).not.toBeInTheDocument();
+  });
+  it('should have the NASA APOD Description', () => {});
 });
