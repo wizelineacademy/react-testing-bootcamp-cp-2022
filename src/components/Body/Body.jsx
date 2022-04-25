e327815cf5669d99f06c911a2553e12831c651e4
Alfonso Ruiz <aerda@MacBook-Pro-de-Alfonso.local>
2022-04-24 20:19:19 -0500

    updating the body component

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,3 +1,17 @@
-const Body = () => <></>;
+const Body = () => (
+  <div>
+    <div>
+      <label htmlFor='apoDate'>Choose the date</label>
+      <input type='date' id='apoDate' name='apoDate'></input>
+    </div>
+    <section>
+      <h1>Image title</h1>
+      <span>apo date</span>
+      <img src='' alt='apo img' />
+    </section>
+
+    <aside></aside>
+  </div>
+);
 
 export default Body;
