a12db25d069db699cb682bdb7ffa45621c04e985
Raul <raulcastellanosh@hotmail.com>
2022-04-24 21:48:35 -0500

    Fix tests to last refactor

--- src/tests/components/main.test.js
+++ src/tests/components/main.test.js
@@ -1,14 +1,24 @@
 import { render, screen } from '@testing-library/react';
+import { MainSection } from '../../components/MainSection';
 
 describe('Test for Main section', () => {
   test('should have a date input element', () => {
     render(<MainSection />);
 
-    const dateInputEl = screen.getByLabelText(/date/i);
+    const dateInputEl = screen.getByLabelText(/pick a date/i);
     expect(dateInputEl).toBeInTheDocument();
   });
 
-  test('should have an image title', () => {});
+  test('should have a submit button', () => {
+    render(<MainSection />);
+
+    const submitButton = screen.getByRole('button', { name: /search/i });
+    expect(submitButton).toBeInTheDocument();
+  });
+
+  test('should have an image title', () => {
+    render(<MainSection />);
+  });
 
   test('should have a loading state if the date changes', () => {});
 
