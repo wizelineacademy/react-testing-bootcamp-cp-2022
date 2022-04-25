351ce85550c90b684e138c2f8f093a26ea6cd819
Ilyeo <jrag.tkd@gmail.com>
2022-04-25 13:12:23 -0500

    Add abstraction to fetch an apod from nasa services
    `getApod` it's a promise that returns the astronomy picture of the day
    (a.k.a. APOD) given a date.

---
+++ src/services/nasa.js
@@ -0,0 +1,21 @@
+export const getApod = async ({ date }) => {
+  try {
+    const url = new URL(process.env.REACT_APP_NASA_APOD_URL)
+
+    url.searchParams.set('date', date)
+
+    const response = await fetch(url)
+
+    if (response.ok) {
+      const data = await response.json()
+
+      return [data, null]
+    } else {
+      const error = await response.json()
+
+      return [null, error]
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
