06f3386641d4e9b56910b5b5dab2e1d12d35e637
candrepa1 <candrepa1@gmail.com>
2022-04-27 16:37:32 -0500

    update tests and add snapshot

--- src/components/DateField/DateField.jsx
+++ src/components/DateField/DateField.jsx
@@ -1,5 +1,41 @@
-const DateField = ({ date }) => {
-	return <input type="date" value={date} data-testid="date-field" readOnly />;
+import styled from "styled-components";
+
+const DateField = ({ date, setDate, handleClick }) => {
+	const handleDateChange = (e) => {
+		setDate(e.target.value);
+	};
+
+	return (
+		<DateFieldContainer>
+			<DatePicker
+				type="date"
+				value={date}
+				data-testid="date-field"
+				onChange={handleDateChange}
+			/>
+			<Button onClick={() => handleClick(date)}>Show</Button>
+		</DateFieldContainer>
+	);
 };
+
+const DateFieldContainer = styled.div`
+	display: flex;
+	justify-content: center;
+`;
+
+const DatePicker = styled.input`
+	padding: 5px;
+`;
+
+const Button = styled.button`
+	padding: 0px 10px;
+	margin-left: 10px;
+	border: 2px solid #b5e1b1;
+	border-radius: 5px;
+	background-color: #f0fbf1;
+	&:hover {
+		background-color: #e4f1e5;
+	}
+`;
 
 export default DateField;
--- src/components/DateField/DateField.test.js
+++ src/components/DateField/DateField.test.js
@@ -5,12 +5,15 @@ import DateField from ".";
 const setUp = (props) => render(<DateField {...props} />);
 
 describe("DateField", () => {
-	it("should render the date field", () => {
-		setUp();
+	it("should render the date field and button", () => {
+		const { container } = setUp();
 
 		const dateField = screen.getByTestId("date-field");
+		const button = screen.getByText(/show/i);
 
 		expect(dateField).toBeInTheDocument();
+		expect(button).toBeInTheDocument();
+		expect(container).toMatchSnapshot();
 	});
 
 	describe("when the date prop is passed", () => {
