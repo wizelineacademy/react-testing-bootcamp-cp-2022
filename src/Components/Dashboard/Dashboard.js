d473591e9c395e98544c36e6ac49a70955e3e8ec
Gabriela <gabriela.razo@painani.mx>
2022-04-27 00:01:08 -0500

    Fourth test, missing the Third.

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,18 +1,27 @@
 import React, { useCallback, useEffect, useState } from "react";
 import moment from 'moment';
 import '../../stylesheets/dashboard.css'
-import {Col } from 'antd';
+import {Col, Spin } from 'antd';
 
 export const Dashboard = () => {
     const [pictureDayInfo, setPictureDayInfo] = useState([])
+    const [fetchingApi, setFetchingApi] = useState(true);
+    const [erroMsg, setErrorMsg] = useState(false);
     const [date, setDate] = useState(moment(new Date()).format("YYYY-MM-DD"))
 
 
     const fetchMyAPI = useCallback(async () => {
-        const apiKey = 'wOjD8BmGH4ZViJyv2OpeZlKfSaA24XkEcvmyipkF';
-        let response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}&start_date=${date}&end_date=${date}`)
-        response = await response.json()
-        setPictureDayInfo(response)
+        if(moment(date).isAfter(moment(new Date()).format("YYYY-MM-DD"))===true || moment('1995-06-16').isSameOrAfter(date)===true ){
+            setErrorMsg(true)
+        }else{
+            const apiKey = 'wOjD8BmGH4ZViJyv2OpeZlKfSaA24XkEcvmyipkF';
+            let response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}&start_date=${date}&end_date=${date}`)
+            response = await response.json();
+            setPictureDayInfo(response)
+            setFetchingApi(false)
+            setErrorMsg(false)
+        }
+        
     }, [date]) // if date changes, useEffect will run again
     
       useEffect(() => {
@@ -21,27 +30,35 @@ export const Dashboard = () => {
 
     return (
       <>
-        {pictureDayInfo[0] ? (
-            <>
-                <br/>
-                <Col span={24} offset={0}>
-                    <div className="container-center-dashboard">
-                        <input
-                            type="date"
-                            value={date}
-                            onChange={event => setDate(event.currentTarget.value)}
-                        />
-                    </div>
-                </Col>
-                <br/>
-                <Col span={18} offset={1}>
-                    <img className="img-dashboard" src={pictureDayInfo[0].url} alt={pictureDayInfo[0].date}/>
-                </Col>
-               
-            </>
-        ) : (
-            <h1>hola</h1>
-        )}
+        <br/>
+        <Col span={24} offset={0}>
+        <form>
+            <label htmlFor="fecha">
+                <div className="container-center-dashboard">
+                    Ingresa una fecha
+                    <input
+                        type="date"
+                        id="fecha" 
+                        name="fecha"
+                        value={date}
+                        onChange={event => {setDate(event.currentTarget.value); setFetchingApi(true)}}
+                    />
+                    {erroMsg === true &&
+                        <p>Date must be between Jun 16, 1995 and Apr 27, 2022.</p>
+                    }
+                </div>
+            </label>
+        </form>
+        </Col>
+        <br/>
+        
+        <Col span={18} offset={1}>
+            <Spin tip="Cargando imagen..." spinning={fetchingApi}>
+            {pictureDayInfo[0] &&
+                <img className="img-dashboard" src={pictureDayInfo[0].url} alt={pictureDayInfo[0].date}/>
+            }
+            </Spin>
+        </Col>
       </>
     )
   }
--- src/Components/Dashboard/dashboard-test.test.js
+++ src/Components/Dashboard/dashboard-test.test.js
@@ -1,5 +1,4 @@
-import {render, screen, fireEvent, getByTestId} from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+import {render, screen, fireEvent, act} from '@testing-library/react'
 import {Dashboard} from './Dashboard'
 import moment from 'moment';
 
@@ -15,11 +14,18 @@ describe('Dasboard', () => {
     })
     it('the app should show the picture of the day for the given date when the user selects a date', async () => {
         setup()
-        const input = await screen.findByLabelText(/input-date/i)
+        const input = await screen.findByLabelText(/fecha/i)
         fireEvent.change(input, {target: {value: '2021-02-12'}})
         const regex = /^\d{4}-\d{2}-\d{2}$/;
         const imgResult = await screen.findByRole('img', {altText: '2021-02-12'})
         expect(input.value).toMatch(regex)
         expect(imgResult).toBeInTheDocument();
+    })
+    it('the app should show a message from the API response when the user selects an invalid date value ', async () => {
+        setup()
+        const input = await screen.findByLabelText(/fecha/i)
+        fireEvent.change(input, {target: {value: '2024-02-12'}})
+        const msgError = screen.getByText(/Date must be between Jun 16, 1995 and Apr 27, 2022./i)
+        expect(msgError).toBeInTheDocument();
     })
 })
