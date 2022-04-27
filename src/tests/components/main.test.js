b4b26ecd75bdaa8906ae87bf2eedce63663cac2d
Raul <raulcastellanosh@hotmail.com>
2022-04-27 12:33:33 -0500

    Refactor main tests

--- src/tests/components/main.test.js
+++ src/tests/components/main.test.js
@@ -10,13 +10,6 @@ import { MainSection } from '../../components/MainSection';
 import testResponse from '../mocks/testResponse.json';
 
 describe('Test for Main section', () => {
-  test('should have a date input element', () => {
-    render(<MainSection />);
-
-    const dateInputEl = screen.getByLabelText(/pick a date/i);
-    expect(dateInputEl).toBeInTheDocument();
-  });
-
   test('should have a submit button', () => {
     render(<MainSection />);
 
@@ -52,5 +45,59 @@ describe('Test for Main section', () => {
   test('should match with snapshot for skeleton purposes', () => {
     const { container } = render(<MainSection />);
     expect(container).toMatchSnapshot();
+  });
+});
+
+describe('Tests on Main wrapper related to dateInput', () => {
+  beforeAll(() => {
+    // add window.matchMedia
+    // this is necessary for the date picker to be rendered in desktop mode.
+    // if this is not provided, the mobile mode is rendered, which might lead to unexpected behavior
+    Object.defineProperty(window, 'matchMedia', {
+      writable: true,
+      value: (query) => ({
+        media: query,
+        // this is the media query that @material-ui/pickers uses to determine if a device is a desktop device
+        matches: query === '(pointer: fine)',
+        onchange: () => {},
+        addEventListener: () => {},
+        removeEventListener: () => {},
+        addListener: () => {},
+        removeListener: () => {},
+        dispatchEvent: () => false,
+      }),
+    });
+  });
+
+  afterAll(() => {
+    delete window.matchMedia;
+  });
+
+  test('should have a date input element', () => {
+    render(<MainSection />);
+
+    const dateInputEl = screen.getByLabelText(/pick a date/i);
+    expect(dateInputEl).toBeInTheDocument();
+  });
+
+  test('should change value of datePicker', () => {
+    render(<MainSection />);
+
+    const dateInputEl = screen.getByLabelText(/Pick a date/i);
+    fireEvent.change(dateInputEl, { target: { value: '01/01/2010' } });
+
+    expect(dateInputEl.value).toBe('01/01/2010');
+  });
+
+  test('should disable the submit button when date is out of range', () => {
+    render(<MainSection />);
+
+    const dateInputEl = screen.getByLabelText(/Pick a date/i);
+    const buttonEl = screen.getByRole('button', { name: /search/i });
+
+    expect(buttonEl).toBeEnabled();
+    fireEvent.change(dateInputEl, { target: { value: '01/01/1500' } });
+    expect(dateInputEl.value).toBe('01/01/1500');
+    expect(buttonEl).toBeDisabled();
   });
 });
