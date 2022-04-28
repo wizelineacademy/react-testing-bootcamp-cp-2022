056770f234898f5a78a8d6e8f53986b8bff50aad
Raul <raulcastellanosh@hotmail.com>
2022-04-28 11:43:10 -0500

    Refactor return respose

--- src/api/apod.js
+++ src/api/apod.js
@@ -2,18 +2,15 @@ const apodPI = process.env.REACT_APP_NASA_API;
 const apodKey = process.env.REACT_APP_NASA_KEY;
 
 export const fetchPictureOfTheDay = async (date) => {
-  let formatedDate = date ? date : new Date();
+  let formatedDate = date || new Date();
   formatedDate = formatedDate.toLocaleDateString('en-CA');
   try {
     const response = await fetch(
       `${apodPI}/planetary/apod?api_key=${apodKey}&date=${formatedDate}`
     );
-    if (response.ok) {
-      return response.json();
-    } else {
-      const res = await response.json();
-      return Promise.reject(res);
-    }
+    const data = await response.json();
+
+    return response.ok ? data : Promise.reject(data);
   } catch (error) {
     throw new Error(error);
   }
