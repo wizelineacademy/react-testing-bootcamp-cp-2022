e920392708ec45dcc5060ecaaba8d0395883bfa7
Luis Gerardo G <luis.garcia@kroger.com>
2022-04-28 21:23:02 -0700

    add Pictures Component

--- src/pages/Pictures.jsx
+++ src/pages/Pictures.jsx
@@ -1,5 +1,51 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { NASA_API_KEY, NASA_BASE_URL } from "../constants";
+
+const pad2 = (str) => str.length < 2 ? `0${str}` : str;
+
 function Pictures() {
-    return <></>;
+    const [date, setDate] = useState(() => {
+        const d = new Date();
+        const year = d.getFullYear();
+        const month = pad2(d.getMonth());
+        const day = pad2(d.getDate());
+        return `${year}-${month}-${day}T00:00`;
+    });
+    const [pictureData, setPictureData] = useState();
+
+    const getData = async () => {
+        const { data } = await axios.get(`${NASA_BASE_URL}`, { params: { api_key: NASA_API_KEY, date } });
+        setPictureData(data);
+        console.log(pictureData);
+    };
+
+    useEffect(() => {
+        getData();
+    }, [date]);
+
+    const handleDateChange = (e) => {
+        const newDate = e.target.value.split('T')[0];
+        setDate(newDate);
+    };
+
+    return (
+        <section>
+            <h1>Picture of the day</h1>
+            <main>
+                <label htmlFor="meeting-time">Choose a date:</label>
+                <input type="datetime-local" id="datetime-local" value={date} onChange={handleDateChange} />
+
+                <h2>{pictureData?.title}</h2>
+                <p>{pictureData?.date}</p>
+                <img aria-label="picture of the day" src={pictureData?.url} />
+                <h3>Explanation</h3>
+                <p>{`${pictureData?.explanation}`}</p>
+                <p>{`Copyright \u00a9 ${pictureData?.copyright}`}</p>
+            </main>
+            <footer>Project created during Wizeline Academy React Testing Bootcamp</footer>
+        </section>
+    );
 }
 
 export default Pictures;
--- src/pages/Pictures.test.jsx
+++ src/pages/Pictures.test.jsx
@@ -1,15 +1,40 @@
+import { render, screen,  } from "@testing-library/react";
+import Pictures from './Pictures';
+
+const setup = () => render(<Pictures />);
+
 describe('Pictures Component', () => {
-    it.todo('should render correctly the header', ()=>{});
+    it('should render correctly the header', ()=>{
+        setup();
+        const headingEl = screen.getByRole('heading', { name: /picture of the day/i });
+
+        expect(headingEl).toBeInTheDocument();
+    });
     
-    it.todo('should render correctly the footer', ()=>{});
+    it('should render correctly the footer', () => {
+        setup();
+        const footerEl = screen.getByRole('contentinfo', { text : /project created during wizeline academy react testing bootcamp/i });
+
+        expect(footerEl).toBeInTheDocument();
+    });
     
-    it.todo('should render correctly the main content', ()=>{});
+    it('should render correctly the main content', () => {
+        setup();
+        const mainEl = screen.getByRole('main');
+
+        expect(mainEl).toBeInTheDocument();
+    });
     
-    it.todo('should show the picture of the day', ()=>{});
+    it('should show the picture of the day', () => {
+        setup();
+        const imageEl = screen.getByRole('img', { name: /picture of the day/i });
+
+        expect(imageEl).toBeInTheDocument();
+    });
     
-    it.todo('should show the picture of the day for the given date', ()=>{});
+    it.todo('should show the picture of the day for the given date');
     
-    it.todo('should show the message: "There was an error, please try again." when there is an unexpected error while fetching the API', ()=>{});
+    it.todo('should show the message: "There was an error, please try again." when there is an unexpected error while fetching the API');
     
-    it.todo('should show a message from the API response (e.g., a day after the current date.), when the user selects an invalid date value and clicks on the show button', ()=>{});
+    it.todo('should show a message from the API response (e.g., a day after the current date.), when the user selects an invalid date value and clicks on the show button');
 });
