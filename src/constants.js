ae9f3025eca01ac2d84e441aa97fd2ddb82314b5
Luis Gerardo G <luis.garcia@kroger.com>
2022-04-28 19:39:05 -0700

    add nasa api constants

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+// example NASA APOD GET request:
+// https://api.nasa.gov/planetary/apod?api_key=cnBU7AEpgeeHVZtx4qykyBUsmxnfghwacmi6urto
+
+export const NASA_BASE_URL = "https://api.nasa.gov/planetary/apod";
+export const NASA_API_KEY = "cnBU7AEpgeeHVZtx4qykyBUsmxnfghwacmi6urto";
