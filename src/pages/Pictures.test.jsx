8a552df371a8215f78921571cdbc11fa352caa0b
Luis Gerardo G <luis.garcia@kroger.com>
2022-04-28 19:40:08 -0700

    add test todos

---
+++ src/pages/Pictures.test.jsx
@@ -0,0 +1,15 @@
+describe('Pictures Component', () => {
+    it.todo('should render correctly the header', ()=>{});
+    
+    it.todo('should render correctly the footer', ()=>{});
+    
+    it.todo('should render correctly the main content', ()=>{});
+    
+    it.todo('should show the picture of the day', ()=>{});
+    
+    it.todo('should show the picture of the day for the given date', ()=>{});
+    
+    it.todo('should show the message: "There was an error, please try again." when there is an unexpected error while fetching the API', ()=>{});
+    
+    it.todo('should show a message from the API response (e.g., a day after the current date.), when the user selects an invalid date value and clicks on the show button', ()=>{});
+});
