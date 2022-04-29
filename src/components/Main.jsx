514ddd9cc5335101e0f07c0431662c9e71b2fca1
gabriel-eb <gabo_espburg@hotmail.com>
2022-04-28 23:01:42 -0500

    First Green Stage pass

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { POD } from "./POD";
+const API_KEY = "aLF9OyI9eUKScjU0GIazR3BKJqntNfay4NXmE6lT";
 
 const style = {
   background: "#A0BCC2",
@@ -15,10 +16,47 @@ const inputStyle = {
 };
 
 export const Main = () => {
+  const [formatedDate, setFormatedDate] = useState("");
+  const [pic, setPic] = useState(null);
+  const [date, setDate] = useState(new Date().toISOString().substring(0, 10));
+
+  useEffect(() => {
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
+      .then((data) => data.json())
+      .then((data) => {
+        setPic(data);
+        setFormatedDate(
+          new Date(data.date).toLocaleDateString("es-MX", {
+            day: "2-digit",
+            month: "2-digit",
+            year: "numeric",
+          })
+        );
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+
   return (
     <div style={style}>
-      <input type="date" name="date" id="date" style={inputStyle} />
-      <POD />
+      <input
+        type="date"
+        name="date"
+        id="date"
+        style={inputStyle}
+        value={date}
+        onChange={(event) => {
+            setDate(new Date(event.target.value).toISOString().substring(0, 10))
+        }}
+      />
+      {pic && (
+        <POD
+          title={pic.title}
+          date={formatedDate}
+          url={pic.url}
+          explanation={pic.explanation}
+        />
+      )}
     </div>
   );
 };
